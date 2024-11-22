a7e004b7a518400c4aaf15c476ae8815e5a7a412
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-11-22 20:09:21 +0500

    start food-menu

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur neque perferendis sunt excepturi corporis unde et laborum, aut maiores. Nam repudiandae labore possimus quaerat ullam placeat animi similique cupiditate suscipit, consequatur tempora magni aliquam doloribus, dolorem at provident sapiente! Iusto nihil impedit repudiandae tenetur? Laborum alias amet laudantium earum. In?
+    </div>
+  );
+};
+
+export default App;
