6fc56d55f5340947ec187e6f094f283e871b326c
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-11-23 12:42:44 +0500

    fixed menu

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,8 +1,12 @@
 import { Button } from "../ui/button";
 import { FaChevronLeft } from "react-icons/fa";
 import Language from "../language";
+import { useStore } from "@/store";
 
 const Navbar = () => {
+    const {changeCardView, viewCard} = useStore()
+    console.log(viewCard);
+    
     return (
         <nav className="flex items-center justify-between px-4 py-3 bg-[#8833EE] text-white">
             <div className="flex items-center gap-3">
@@ -14,14 +18,14 @@ const Navbar = () => {
             </div>
 
             <div className="flex items-center gap-4">
-                <div className="flex items-center gap-4 p-1 px-2 bg-white/20 rounded-md">
-                    <Button variant="ghost" size="icon" className="text-[#8833EE] bg-white flex flex-col">
+                <div className="flex items-center w-[100px] h-[40px] bg-white/20 rounded-md justify-between">
+                    <div onClick={() => changeCardView('row')} className={`h-full rounded-md cursor-pointer flex items-center justify-center px-3 flex-col space-y-1 ${viewCard=='row'?'bg-white':''}`}>
                         <span className="blog p-[6px] px-4 bg-[#8833EE] rounded-md"></span>
                         <span className="blog p-[6px] px-4 bg-[#8833EE] rounded-md"></span>
-                    </Button>
-                    <Button variant="ghost" size="icon" className="">
-                        <span className="blog p-[18px] bg-white/40 rounded-sm"></span>
-                    </Button>
+                    </div>
+                    <div onClick={() => changeCardView('col')} className={`h-full px-[9px] flex justify-center items-center rounded-md cursor-pointer ${viewCard=='col'?'bg-white':''}`}>
+                        <span className={`inline-block h-[25px] w-[25px] px-[10px] rounded-sm z-50 ${viewCard=='col'?'bg-[#8833EE]':'bg-white/20'}`}></span>
+                    </div>
                 </div>
                 <Language />
             </div>
